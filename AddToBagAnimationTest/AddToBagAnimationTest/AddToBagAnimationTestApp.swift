e7b0f1b071438ac5b42837a25a284a5f879bdf64
samgusa <45985527+samgusa@users.noreply.github.com>
2023-06-14 21:32:48 -0400

    Add files via upload

---
+++ AddToBagAnimationTest/AddToBagAnimationTest/AddToBagAnimationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddToBagAnimationTestApp.swift
+//  AddToBagAnimationTest
+//
+//  Created by Sam Greenhill on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AddToBagAnimationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
